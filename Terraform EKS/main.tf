5ba9b88202312947b82a0d49a3ba761185c64bdc
LouisFifield <fifield@live.co.uk>
2023-09-22 11:42:19 +0100

    Add: extra nodes to cluster

--- Terraform EKS/main.tf
+++ Terraform EKS/main.tf
@@ -14,3 +14,21 @@ module "eks_cluster" {
   private_subnets = module.networking.private_subnets
   cluster_name    = var.cluster_name
 }
+
+resource "null_resource" "update_desired_size" {
+  triggers = {
+    desired_size = 2
+  }
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+
+    # Note: this requires the awscli to be installed locally where Terraform is executed
+    command = <<-EOT
+      aws eks update-nodegroup-config \
+        --cluster-name ${module.eks_cluster.cluster_name} \
+        --nodegroup-name ${element(split(":", module.eks_cluster.eks_managed_node_groups["one"].node_group_id), 1)} \
+        --scaling-config desiredSize=2 \
+        --region eu-west-2
+    EOT
+  }
+}
--- Terraform EKS/modules/containerisation/main.tf
+++ Terraform EKS/modules/containerisation/main.tf
@@ -22,9 +22,9 @@ module "eks" {
 
       instance_types = ["t3.small"]
 
+      desired_size = 2
       min_size     = 1
-      max_size     = 1
-      desired_size = 1
+      max_size     = 3
     }
   }
 }
